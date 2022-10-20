581007ec89f0d5ae5cf5c1e4b7fe4cb3e49f8f25
OfirYC <ofiryieldchain@gmail.com>
2022-10-20 10:50:35 +0300

    commit

--- contracts/kahoot.sol
+++ contracts/kahoot.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
+// import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
 
 contract kahoot {
     // Variables
